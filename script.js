fc6448ac6f41d2c2f986bc3d39ae55641c9d3145
Karthik Sunkari <84893245+Karthiksunkari3@users.noreply.github.com>
2023-05-18 18:52:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 function createTable() {
-    //Write your code here
-  
+  const numRows = parseInt(prompt('Input number of rows'));
+  const numColumns = parseInt(prompt('Input number of columns'));
+
+  const table = document.getElementById('myTable');
+
+  // Clear any existing table content
+  table.innerHTML = '';
+
+  // Create rows and columns
+  for (let i = 0; i < numRows; i++) {
+    const row = table.insertRow(i);
+
+    for (let j = 0; j < numColumns; j++) {
+      const cell = row.insertCell(j);
+      cell.textContent = `Row-${i} Column-${j}`;
+    }
+  }
 }
